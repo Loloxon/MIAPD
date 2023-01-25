1e70b3ba4ea1bdf0da5e1529a5dc2fbf8b1c23c5
bblachut1 <bogojedi1@gmail.com>
2023-01-25 17:39:08 +0100

    add path to main package

--- Project/main.py
+++ Project/main.py
@@ -1,6 +1,9 @@
 from Project.gui.GUI import GUI
 from Project.resources.DataBase import DataBase
+import sys
+import os
 
+sys.path.insert(1, os.path.join(sys.path[0], '..'))
 
 if __name__ == '__main__':
     db = DataBase()
