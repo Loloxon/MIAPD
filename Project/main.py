28ab2e21aa3307a7d4f34246081632710fb512b9
bblachut1 <bogojedi1@gmail.com>
2023-01-27 10:35:06 +0100

    fix path inserting

--- Project/main.py
+++ Project/main.py
@@ -1,9 +1,11 @@
-from Project.gui.GUI import GUI
-from Project.resources.DataBase import DataBase
 import sys
 import os
-
 sys.path.insert(1, os.path.join(sys.path[0], '..'))
+from Project.gui.GUI import GUI
+from Project.resources.DataBase import DataBase
+
+
+
 
 if __name__ == '__main__':
     db = DataBase()
